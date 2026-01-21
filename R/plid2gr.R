59e7531249cd75bd88e8c2f0481a0a121914f42d
stvjc <stvjc@channing.harvard.edu>
2026-01-21 00:45:41 -0500

    generalized plid2gr to handle different separators ch:pos:... vs ch_pos_...

--- R/plid2gr.R
+++ R/plid2gr.R
@@ -2,11 +2,12 @@
 #' @import GenomicRanges
 #' @import IRanges
 #' @param x character vector of variant names
+#' @param sepused single character, defaults to "_"
 #' @return GRanges instance
 #' @export
-plid2gr = function (x) 
+plid2gr = function (x, sepused="_") 
 {
-    ss = strsplit(x, "_")
+    ss = strsplit(x, sepused)
     pos = as.integer(sapply(ss, "[", 2))
     ch = sapply(ss, "[", 1)
     ans = GRanges(ch, IRanges(pos, width = 1))
