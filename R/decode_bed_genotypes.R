90adf6515a28a65fc59d5342f02f50c7a00fb4f5
vjcitn <stvjc@channing.harvard.edu>
2026-03-31 16:19:16 -0400

    bump version and deal with decode_bed example request

--- R/decode_bed_genotypes.R
+++ R/decode_bed_genotypes.R
@@ -2,7 +2,7 @@
 #' @param raw_bytes byte stream from file
 #' @param n_samples number of samples
 #' @examples
-#' decode_bed_genotypes
+#' PlinkMatrix:::decode_bed_genotypes(raw(4), 4L)
 #' @keywords internal
 decode_bed_genotypes <- function(raw_bytes, n_samples) {
   # Lookup table for decoding (NOW COUNTING A1 ALLELE)
